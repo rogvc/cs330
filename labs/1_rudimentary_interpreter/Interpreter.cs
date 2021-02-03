b3284596b9369d542057c1f75a94c98f7ca215cc
Rogerio Cruz <rogvc@outlook.com>
2021-02-02 22:08:38 -0700

    Initial Rudimentary Interpreter Setup  - Adds initial c# dotnet project;

---
+++ labs/1_rudimentary_interpreter/Interpreter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RudimentaryInterpreter
+{
+    class Interpreter
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ labs/README.md
@@ -0,0 +1,4 @@
+# Labs for CS 330
+Here you will find all the labs I've done for this class.  
+
+See each directory for more information on the lab and its purpose.
